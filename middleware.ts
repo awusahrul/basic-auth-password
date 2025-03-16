24d0dfb92122333b429e59a96009d81cfc1ab42e
Sahrul <86223518+awusahrul@users.noreply.github.com>
2025-03-16 12:02:49 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,27 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: '/:path*', // Semua halaman harus login
+  matcher: '/:path*', // Mengunci semua halaman
 }
 
 export default function middleware(req: NextRequest) {
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
+  // Mengambil username dan password dari environment variables
+  const ADMIN_USER = process.env.ADMIN_USER || 'admin' // Default jika tidak di-set
+  const ADMIN_PASS = process.env.ADMIN_PASS || 'admin' // Default jika tidak di-set
+
   if (basicAuth) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
 
-    // Ubah username dan password menjadi 'admin'
-    if (user === 'admin' && pwd === 'admin') {
+    // Membandingkan dengan environment variables
+    if (user === ADMIN_USER && pwd === ADMIN_PASS) {
       return NextResponse.next()
     }
   }
 
-  url.pathname = '/api/auth' // Redirect ke halaman login jika tidak terautentikasi
+  url.pathname = '/api/auth' // Redirect ke halaman login jika belum login
   return NextResponse.rewrite(url)
 }
