051a9787ecc53781416ad4d26830d4c3e977fc40
Sahrul <86223518+awusahrul@users.noreply.github.com>
2025-03-16 12:25:28 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,24 @@
+import { Layout, Page, Text, Button } from '@vercel/examples-ui'
+import { useRouter } from 'next/router'
+
+export default function Index() {
+  const router = useRouter()
+
+  const handleLogout = () => {
+    document.cookie = 'Authorization=; Max-Age=0; path=/;' // Hapus cookie auth
+    router.replace('/api/auth') // Redirect ke halaman login
+  }
+
+  return (
+    <Page>
+      <Text variant="h2" className="mb-6">
+        Login Berhasil
+      </Text>
+      <Text className="mb-4">Selamat datang! Anda telah berhasil login.</Text>
+      <Button onClick={handleLogout}>Logout</Button>
+    </Page>
+  )
+}
+
+Index.Layout = Layout
 
