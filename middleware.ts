1761fb463dcc21b46a4b25cdb488b33cc1160352
Sahrul <86223518+awusahrul@users.noreply.github.com>
2025-03-17 13:21:27 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: [
-    '/:path*', // Mengunci semua halaman
-    // Kecualikan path yang dimulai dengan /page/api/cloud/
-    '!/page/api/cloud/:path*',
-  ],
+  matcher: '/:path*', // Mengunci semua halaman
 };
 
 export default function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
+  const url = req.nextUrl;
+
+  // Lewati autentikasi untuk path yang dimulai dengan /page/api/cloud/
+  if (url.pathname.startsWith('/api/cloud/library/')) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get('authorization');
 
   // Mengambil username dan password dari environment variables
-  const ADMIN_USER = process.env.ADMIN_USER || 'admin' // Default jika tidak di-set
-  const ADMIN_PASS = process.env.ADMIN_PASS || 'admin' // Default jika tidak di-set
+  const ADMIN_USER = process.env.ADMIN_USER || 'admin'; // Default jika tidak di-set
+  const ADMIN_PASS = process.env.ADMIN_PASS || 'admin'; // Default jika tidak di-set
 
   if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
 
     // Membandingkan dengan environment variables
     if (user === ADMIN_USER && pwd === ADMIN_PASS) {
-      return NextResponse.next()
+      return NextResponse.next();
     }
   }
 
-  url.pathname = '/api/auth' // Redirect ke halaman login jika belum login
-  return NextResponse.rewrite(url)
+  url.pathname = '/api/auth'; // Redirect ke halaman login jika belum login
+  return NextResponse.rewrite(url);
 }
